1a369e0b53fb7ccd9fd9c6901adf5a554cdf1e5d
Hancheng Zhang <hz14899@it052957.users.bris.ac.uk>
2018-01-29 15:51:37 +0000

    2.3 TODO

--- skeleton.cpp
+++ skeleton.cpp
@@ -1,112 +1,189 @@
-#include <iostream>
+#ifndef TEST_MODEL_CORNEL_BOX_H
+#define TEST_MODEL_CORNEL_BOX_H
+
+// Defines a simple test model: The Cornel Box
+
 #include <glm/glm.hpp>
-#include <SDL.h>
-#include "SDLauxiliary.h"
-#include "TestModelH.h"
-#include <stdint.h>
+#include <vector>
 
-using namespace std;
-using glm::vec3;
-using glm::mat3;
-using glm::vec4;
-using glm::mat4;
-
-
-#define SCREEN_WIDTH 320
-#define SCREEN_HEIGHT 256
-#define FULLSCREEN_MODE false
-
-/* ----------------------------------------------------------------------------*/
-/* FUNCTIONS                                                                   */
-
-void Update();
-void Draw(screen* screen);
-int main( int argc, char* argv[] )
+// Used to describe a triangular surface:
+class Triangle
 {
+public:
+  glm::vec4 v0;
+  glm::vec4 v1;
+  glm::vec4 v2;
+  glm::vec4 normal;
+  glm::vec3 color;
 
-  screen *screen = InitializeSDL( SCREEN_WIDTH, SCREEN_HEIGHT, FULLSCREEN_MODE );
-  
-  vector<Triangle> Triangles;
-  LoadTestModel(Triangles);
-
-
-
-
-  while( NoQuitMessageSDL() )
+  Triangle( glm::vec4 v0, glm::vec4 v1, glm::vec4 v2, glm::vec3 color )
+    : v0(v0), v1(v1), v2(v2), color(color)
   {
-    Update();
-    Draw(screen);
-    SDL_Renderframe(screen);
+    ComputeNormal();
   }
 
-  SDL_SaveImage( screen, "screenshot.bmp" );
-
-  KillSDL(screen);
-  return 0;
-}
-
-/*Place your drawing here*/
-void Draw(screen* screen)
-{
-  /* Clear buffer */
-  memset(screen->buffer, 0, screen->height*screen->width*sizeof(uint32_t));
-  
-  vec3 colour(1.0,0.0,0.0);
-  for(int i=0; i<1000; i++)
+  void ComputeNormal()
   {
-    uint32_t x = rand() % screen->width;
-    uint32_t y = rand() % screen->height;
-    PutPixelSDL(screen, x, y, colour);
+    glm::vec3 e1 = glm::vec3(v1.x-v0.x,v1.y-v0.y,v1.z-v0.z);
+    glm::vec3 e2 = glm::vec3(v2.x-v0.x,v2.y-v0.y,v2.z-v0.z);
+    glm::vec3 normal3 = glm::normalize( glm::cross( e2, e1 ) );
+    normal.x = normal3.x;
+    normal.y = normal3.y;
+    normal.z = normal3.z;
+    normal.w = 1.0;
+  }
+};
 
+// Loads the Cornell Box. It is scaled to fill the volume:
+// -1 <= x <= +1
+// -1 <= y <= +1
+// -1 <= z <= +1
+void LoadTestModel( std::vector<Triangle>& triangles )
+{
+  using glm::vec3;
+  using glm::vec4;
+
+  // Defines colors:
+  vec3 red(    0.75f, 0.15f, 0.15f );
+  vec3 yellow( 0.75f, 0.75f, 0.15f );
+  vec3 green(  0.15f, 0.75f, 0.15f );
+  vec3 cyan(   0.15f, 0.75f, 0.75f );
+  vec3 blue(   0.15f, 0.15f, 0.75f );
+  vec3 purple( 0.75f, 0.15f, 0.75f );
+  vec3 white(  0.75f, 0.75f, 0.75f );
+
+  triangles.clear();
+  triangles.reserve( 5*2*3 );
+
+  // ---------------------------------------------------------------------------
+  // Room
+
+  float L = 555;      // Length of Cornell Box side.
+
+  vec4 A(L,0,0,1);
+  vec4 B(0,0,0,1);
+  vec4 C(L,0,L,1);
+  vec4 D(0,0,L,1);
+
+  vec4 E(L,L,0,1);
+  vec4 F(0,L,0,1);
+  vec4 G(L,L,L,1);
+  vec4 H(0,L,L,1);
+
+  // Floor:
+  triangles.push_back( Triangle( C, B, A, green ) );
+  triangles.push_back( Triangle( C, D, B, green ) );
+
+  // Left wall
+  triangles.push_back( Triangle( A, E, C, purple ) );
+  triangles.push_back( Triangle( C, E, G, purple ) );
+
+  // Right wall
+  triangles.push_back( Triangle( F, B, D, yellow ) );
+  triangles.push_back( Triangle( H, F, D, yellow ) );
+
+  // Ceiling
+  triangles.push_back( Triangle( E, F, G, cyan ) );
+  triangles.push_back( Triangle( F, H, G, cyan ) );
+
+  // Back wall
+  triangles.push_back( Triangle( G, D, C, white ) );
+  triangles.push_back( Triangle( G, H, D, white ) );
+
+  // ---------------------------------------------------------------------------
+  // Short block
+
+  A = vec4(290,0,114,1);
+  B = vec4(130,0, 65,1);
+  C = vec4(240,0,272,1);
+  D = vec4( 82,0,225,1);
+         
+  E = vec4(290,165,114,1);
+  F = vec4(130,165, 65,1);
+  G = vec4(240,165,272,1);
+  H = vec4( 82,165,225,1);
+
+  // Front
+  triangles.push_back( Triangle(E,B,A,red) );
+  triangles.push_back( Triangle(E,F,B,red) );
+
+  // Front
+  triangles.push_back( Triangle(F,D,B,red) );
+  triangles.push_back( Triangle(F,H,D,red) );
+
+  // BACK
+  triangles.push_back( Triangle(H,C,D,red) );
+  triangles.push_back( Triangle(H,G,C,red) );
+
+  // LEFT
+  triangles.push_back( Triangle(G,E,C,red) );
+  triangles.push_back( Triangle(E,A,C,red) );
+
+  // TOP
+  triangles.push_back( Triangle(G,F,E,red) );
+  triangles.push_back( Triangle(G,H,F,red) );
+
+  // ---------------------------------------------------------------------------
+  // Tall block
+
+  A = vec4(423,0,247,1);
+  B = vec4(265,0,296,1);
+  C = vec4(472,0,406,1);
+  D = vec4(314,0,456,1);
+         
+  E = vec4(423,330,247,1);
+  F = vec4(265,330,296,1);
+  G = vec4(472,330,406,1);
+  H = vec4(314,330,456,1);
+
+  // Front
+  triangles.push_back( Triangle(E,B,A,blue) );
+  triangles.push_back( Triangle(E,F,B,blue) );
+
+  // Front
+  triangles.push_back( Triangle(F,D,B,blue) );
+  triangles.push_back( Triangle(F,H,D,blue) );
+
+  // BACK
+  triangles.push_back( Triangle(H,C,D,blue) );
+  triangles.push_back( Triangle(H,G,C,blue) );
+
+  // LEFT
+  triangles.push_back( Triangle(G,E,C,blue) );
+  triangles.push_back( Triangle(E,A,C,blue) );
+
+  // TOP
+  triangles.push_back( Triangle(G,F,E,blue) );
+  triangles.push_back( Triangle(G,H,F,blue) );
+
+
+  // ----------------------------------------------
+  // Scale to the volume [-1,1]^3
+
+  for( size_t i=0; i<triangles.size(); ++i )
+  {
+    triangles[i].v0 *= 2/L;
+    triangles[i].v1 *= 2/L;
+    triangles[i].v2 *= 2/L;
+
+    triangles[i].v0 -= vec4(1,1,1,1);
+    triangles[i].v1 -= vec4(1,1,1,1);
+    triangles[i].v2 -= vec4(1,1,1,1);
+
+    triangles[i].v0.x *= -1;
+    triangles[i].v1.x *= -1;
+    triangles[i].v2.x *= -1;
+
+    triangles[i].v0.y *= -1;
+    triangles[i].v1.y *= -1;
+    triangles[i].v2.y *= -1;
+
+    triangles[i].v0.w = 1.0;
+    triangles[i].v1.w = 1.0;
+    triangles[i].v2.w = 1.0;
+    
+    triangles[i].ComputeNormal();
   }
 }
 
-/*Place updates of parameters here*/
-void Update()
-{
-  static int t = SDL_GetTicks();
-  /* Compute frame time */
-  int t2 = SDL_GetTicks();
-  float dt = float(t2-t);
-  t = t2;
-  /*Good idea to remove this*/
-  std::cout << "Render time: " << dt << " ms." << std::endl;
-  /* Update variables*/
-}
-
-
-// void pointOnTriangle(Triangle x, float u, float v)
-// {
-//   vec4 e1, e2, r;
-//   e1 = x.v1 - x.v0;
-//   e2 = x.v2 - x.v0;
-//   r = x.v0 + u*e1 +v*e2;
-// }
-
-  struct Intersection
-  {
-    vec4 position;
-    float distance;
-    int triangleIndex;
-  };
-
-
-bool ClosestIntersection(vec4 s, vec4 d, 
-  const vector<Triangle>& triangles, 
-  Intersection& closestIntersection )
-{
-
-  for(unsigned int i; i < triangles.size(); i++)
-    {
-      vec4 v0 = triangles[i].v0;
-      vec4 v1 = triangles[i].v1;
-      vec4 v2 = triangles[i].v2;
-      vec3 e1 = vec3(v1.x-v0.x,v1.y-v0.y,v1.z-v0.z);
-      vec3 e2 = vec3(v2.x-v0.x,v2.y-v0.y,v2.z-v0.z);
-      vec3 b = vec3(s.x-v0.x,s.y-v0.y,s.z-v0.z);
-      mat3 A( vec3(d[0], d[1], d[2]), e1, e2 );
-      vec3 x = glm::inverse( A ) * b;
-    }
-    return true;
-}
-
+#endif
